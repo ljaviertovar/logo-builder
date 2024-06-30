895628edd12108cb3e81d636f51c8cde9cdc2565
ljaviertovar <luisjavier_tovar@outlook.com>
2024-06-30 00:15:48 -0400

    add context

--- src/components/color-picker-controller.tsx
+++ src/components/color-picker-controller.tsx
@@ -1,4 +1,5 @@
-import { useState } from 'react'
+import { useAppContext } from '@/context'
+import { useEffect, useState } from 'react'
 
 import ColorPicker from 'react-best-gradient-color-picker'
 
@@ -8,7 +9,13 @@ interface Props {
 }
 
 export default function ColorPickerController({ hidecontrols = true, hidepresets = true }: Props) {
-	const [color, setColor] = useState('#ff0000')
+	const { values, setValues } = useAppContext()
+
+	const [color, setColor] = useState(values.iconColor)
+
+	useEffect(() => {
+		setValues({ ...values, iconColor: color })
+	}, [color])
 
 	return (
 		<div className='flex '>
